0cadfae80b21cb69733bf808b2aca7982908cb45
Miranda <andre.miranda@araujo.com.br>
2026-04-29 19:19:01 -0300

    feat: estrutura inicial do projeto

---
+++ consumer/src/test/java/com/andre/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
